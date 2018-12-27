37193a03dfb31ed6ab8f4a704637790c9dd17eef
Dan Brumleve <jdb1729@gmail.com>
2018-12-26 21:45:42 -0600

    project class outline

---
+++ project.cc
@@ -0,0 +1,8 @@
+#define __MAKEMORE_PROJECT_CC__ 1
+#include "project.hh"
+
+Project::Project() {
+}
+
+Project::~Project() {
+}
---
+++ project.hh
@@ -0,0 +1,10 @@
+#ifndef __MAKEMORE_PROJECT_HH__
+#define __MAKEMORE_PROJECT_HH__ 1
+
+struct Project {
+  Project();
+  ~Project();
+};
+
+#endif
+
