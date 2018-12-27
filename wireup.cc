5b6ca1c4830c85c36df9c0cde20683afd62836ad
Dan Brumleve <jdb1729@gmail.com>
2018-12-27 10:08:41 -0600

    tool to create wiring

---
+++ wireup.cc
@@ -0,0 +1,24 @@
+#include "wiring.hh"
+#include "layout.hh"
+
+int main(int argc, char **argv) {
+  assert(argc >= 3);
+  const char *ifn = argv[1];
+  const char *ofn = argv[2];
+
+  unsigned int minv = 0;
+  if (argc >= 4)
+    minv = (unsigned int)atoi(argv[3]);
+
+  unsigned int maxv = 0;
+  if (argc >= 5)
+    maxv = (unsigned int)atoi(argv[4]);
+
+  Layout *il = load_new<Layout>(ifn);
+  Layout *ol = load_new<Layout>(ofn);
+
+  Wiring *wire = new Wiring(il, ol, minv, maxv);
+  wire->save(stdout);
+  return 0;
+}
+
