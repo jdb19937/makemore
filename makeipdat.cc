a778ee9c15f3b9536b6a1d9c050b4efb65c5b1de
Dan Brumleve <jdb1729@gmail.com>
2019-01-27 22:33:00 -0600

    ip stats db initializer

---
+++ makeipdat.cc
@@ -0,0 +1,14 @@
+#include <assert.h>
+
+#include "ipdb.hh"
+
+int main(int argc, char **argv) {
+  assert(argc == 3);
+  const char *ips_fn = argv[1];
+  unsigned int n_ips = (unsigned)atoi(argv[2]);
+
+  fprintf(stderr, "creating ips_fn=%s n_ips=%u\n", ips_fn, n_ips);
+  IPDB::create(ips_fn, n_ips);
+  fprintf(stderr, "created ips_fn=%s n_ips=%u\n", ips_fn, n_ips);
+  return 0;
+}
